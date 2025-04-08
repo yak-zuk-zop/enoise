6aaa10c3e1c84747124975dd4a1e1faf9fb908e5
Yuri Bukhalenkov <yuri.bukhalenkov@gmail.com>
2025-04-08 19:40:19 +0300

    rewrote handshake state tests. now it tests it's api (not handshake patterns)

--- test/enoise_hs_state_tests.erl
+++ test/enoise_hs_state_tests.erl
@@ -4,62 +4,103 @@
 
 -spec test() -> _.
 
-%%-- fixtures -----------------------------------------------------------------
-
--spec noise_hs_test_() -> _.
-noise_hs_test_() ->
-    {setup,
-        fun() -> test_utils:noise_test_vectors(fun test_utils:protocol_filter_interactive/1) end,
-        fun(Tests) ->
-            TestCaseFun = fun(T) -> test_utils:init_hs_test(T, fun noise_test/5) end,
-            [{maps:get(protocol_name, T), {with, T, [TestCaseFun]}} || T <- Tests]
-        end
-    }.
-
 %%-- tests --------------------------------------------------------------------
 
-noise_test(Protocol, Init, Resp, Messages, HSHash) ->
-    DH = enoise_protocol:dh(Protocol),
-    NewK = fun(Key) -> test_utils:maybe_new_keypair(DH, Key) end,
-    HSInit = fun(#{e := E, s := S, rs := RS, prologue := PL}, R) ->
-        Keys = {NewK({secret, S}), NewK({secret, E}), NewK({public, RS}), undefined},
-        enoise_hs_state:init(Protocol, R, PL, Keys)
-    end,
-    InitHS = HSInit(Init, initiator),
-    RespHS = HSInit(Resp, responder),
+-spec empty_prologue_test() -> _.
+empty_prologue_test() ->
+    Protocol = enoise_protocol:from_name(<<"Noise_NN_25519_ChaChaPoly_SHA256">>),
+    InitialKeypairs = {undefined, undefined, undefined, undefined},
 
-    noise_test(Messages, InitHS, RespHS, HSHash).
+    AliceHS0 = enoise_hs_state:init(Protocol, initiator, <<>>, InitialKeypairs),
+    BobHS0 = enoise_hs_state:init(Protocol, responder, <<>>, InitialKeypairs),
 
-%%-- internals ----------------------------------------------------------------
+    ?assertEqual(out, enoise_hs_state:next_message(AliceHS0)),
+    {ok, AliceHS1, EncMsg1} = enoise_hs_state:write_message(AliceHS0, <<>>),
+    ?assertEqual(in, enoise_hs_state:next_message(BobHS0)),
+    {ok, BobHS1, <<>>} = enoise_hs_state:read_message(BobHS0, EncMsg1),
 
-noise_test([M = #{payload := PL0, ciphertext := CT0} | Msgs], SendHS, RecvHS, HSHash) ->
-    case {enoise_hs_state:next_message(SendHS), enoise_hs_state:next_message(RecvHS)} of
-        {out, in} ->
-            PL = test_utils:hex2bin(<<$0, $x, PL0/binary>>),
-            CT = test_utils:hex2bin(<<$0, $x, CT0/binary>>),
+    ?assertEqual(out, enoise_hs_state:next_message(BobHS1)),
+    {ok, BobHS2, EncMsg2} = enoise_hs_state:write_message(BobHS1, <<>>),
+    {ok, BobSplitState} = enoise_hs_state:finalize(BobHS2),
 
-            {ok, SendHS1, Message} = enoise_hs_state:write_message(SendHS, PL),
-            ?assertEqual(CT, Message),
-            {ok, RecvHS1, PL1} = enoise_hs_state:read_message(RecvHS, Message),
-            ?assertEqual(PL, PL1),
-            noise_test(Msgs, RecvHS1, SendHS1, HSHash);
-        {done, done} ->
-            {ok, #{rx := RX1, tx := TX1, hs_hash := HSHash1}} = enoise_hs_state:finalize(SendHS),
-            {ok, #{rx := RX2, tx := TX2, hs_hash := HSHash2}} = enoise_hs_state:finalize(RecvHS),
-            ?assertEqual(RX1, TX2), ?assertEqual(RX2, TX1),
-            ?assertEqual(HSHash, HSHash1), ?assertEqual(HSHash, HSHash2),
-            message_test([M | Msgs], TX1, RX1);
-        {Out, In} ->
-            ?assertMatch({out, in}, {Out, In})
-    end.
+    ?assertEqual(in, enoise_hs_state:next_message(AliceHS1)),
+    {ok, AliceHS2, <<>>} = enoise_hs_state:read_message(AliceHS1, EncMsg2),
+    {ok, AliceSplitState} = enoise_hs_state:finalize(AliceHS2),
 
-message_test([], _, _) -> ok;
-message_test([#{payload := PL0, ciphertext := CT0} | Msgs], CA, CB) ->
-    PL = test_utils:hex2bin(<<$0, $x, PL0/binary>>),
-    CT = test_utils:hex2bin(<<$0, $x, CT0/binary>>),
-    {ok, CA1, CT1} = enoise_cipher_state:encrypt_with_ad(CA, <<>>, PL),
-    ?assertEqual(CT, CT1),
-    {ok, CA2, PL1} = enoise_cipher_state:decrypt_with_ad(CA, <<>>, CT1),
-    ?assertEqual(CA1, CA2),
-    ?assertEqual(PL, PL1),
-    message_test(Msgs, CB, CA1).
+    #{rx := AliceRx, tx := AliceTx, hs_hash := AliceHSHash} = AliceSplitState,
+    #{rx := BobRx, tx := BobTx, hs_hash := BobHSHash} = BobSplitState,
+    ?assertEqual(AliceRx, BobTx),
+    ?assertEqual(BobRx, AliceTx),
+    ?assertEqual(AliceHSHash, BobHSHash).
+
+-spec prologue_test() -> _.
+prologue_test() ->
+    Protocol = enoise_protocol:from_name(<<"Noise_NN_25519_ChaChaPoly_SHA256">>),
+    Prologue = <<"Pr010gUe">>,
+    InitialKeypairs = {undefined, undefined, undefined, undefined},
+
+    AliceHS0 = enoise_hs_state:init(Protocol, initiator, Prologue, InitialKeypairs),
+    BobHS0 = enoise_hs_state:init(Protocol, responder, Prologue, InitialKeypairs),
+
+    ?assertEqual(out, enoise_hs_state:next_message(AliceHS0)),
+    {ok, AliceHS1, EncMsg1} = enoise_hs_state:write_message(AliceHS0, <<>>),
+    ?assertEqual(in, enoise_hs_state:next_message(BobHS0)),
+    {ok, BobHS1, <<>>} = enoise_hs_state:read_message(BobHS0, EncMsg1),
+
+    ?assertEqual(out, enoise_hs_state:next_message(BobHS1)),
+    {ok, BobHS2, EncMsg2} = enoise_hs_state:write_message(BobHS1, <<>>),
+    {ok, BobSplitState} = enoise_hs_state:finalize(BobHS2),
+
+    ?assertEqual(in, enoise_hs_state:next_message(AliceHS1)),
+    {ok, AliceHS2, <<>>} = enoise_hs_state:read_message(AliceHS1, EncMsg2),
+    {ok, AliceSplitState} = enoise_hs_state:finalize(AliceHS2),
+
+    #{rx := AliceRx, tx := AliceTx, hs_hash := AliceHSHash} = AliceSplitState,
+    #{rx := BobRx, tx := BobTx, hs_hash := BobHSHash} = BobSplitState,
+    ?assertEqual(AliceRx, BobTx),
+    ?assertEqual(BobRx, AliceTx),
+    ?assertEqual(AliceHSHash, BobHSHash).
+
+-spec substituted_prologue_test() -> _.
+substituted_prologue_test() ->
+    Protocol = enoise_protocol:from_name(<<"Noise_NN_25519_AESGCM_SHA256">>),
+    InitialKeypairs = {undefined, undefined, undefined, undefined},
+
+    AliceHS0 = enoise_hs_state:init(Protocol, initiator, <<"Pr010gUe">>, InitialKeypairs),
+    %% Mallory replaced prologue
+    BobHS0 = enoise_hs_state:init(Protocol, responder, <<"Pr010g">>, InitialKeypairs),
+
+    {ok, AliceHS1, EncMsg1} = enoise_hs_state:write_message(AliceHS0, <<>>),
+    {ok, BobHS1, <<>>} = enoise_hs_state:read_message(BobHS0, EncMsg1),
+
+    {ok, BobHS2, EncMsg2} = enoise_hs_state:write_message(BobHS1, <<>>),
+    {ok, _BobSplitState} = enoise_hs_state:finalize(BobHS2),
+
+    ?assertMatch({error, _}, enoise_hs_state:read_message(AliceHS1, EncMsg2)).
+
+-spec unexpected_finalize_step_test() -> _.
+unexpected_finalize_step_test() ->
+    Protocol = enoise_protocol:from_name(<<"Noise_NN_25519_AESGCM_SHA256">>),
+    InitialKeypairs = {undefined, undefined, undefined, undefined},
+
+    HS = enoise_hs_state:init(Protocol, initiator, <<>>, InitialKeypairs),
+
+    ?assertError({expected, out}, enoise_hs_state:finalize(HS)).
+
+-spec unexpected_read_test() -> _.
+unexpected_read_test() ->
+    Protocol = enoise_protocol:from_name(<<"Noise_NN_25519_AESGCM_SHA256">>),
+    InitialKeypairs = {undefined, undefined, undefined, undefined},
+
+    HS = enoise_hs_state:init(Protocol, initiator, <<>>, InitialKeypairs),
+
+    ?assertEqual({error, {expected, out}}, enoise_hs_state:read_message(HS, <<>>)).
+
+-spec unexpected_write_test() -> _.
+unexpected_write_test() ->
+    Protocol = enoise_protocol:from_name(<<"Noise_NN_25519_AESGCM_SHA256">>),
+    InitialKeypairs = {undefined, undefined, undefined, undefined},
+
+    HS = enoise_hs_state:init(Protocol, responder, <<>>, InitialKeypairs),
+
+    ?assertEqual({error, {expected, in}}, enoise_hs_state:write_message(HS, <<>>)).
